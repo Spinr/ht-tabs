7aee00342e959538340baa92bb207bfeb4b39cdb
Rsteenwijk <rsteenwijk@versplatformnederland.nl>
2016-05-24 22:06:27 +0200

    Installed Semver and made node version check

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,26 @@
 (function() {
     "use strict";
 
+    var semver = require("semver");
 
     desc("Default build");
-    task("default", function(){
+    task("default", ["version"], function(){
         console.log("BUILD OK!!!");
     });
+
+    desc("Check node version");
+    task("version",function(){
+        console.log("Checking Node version: ...");
+        var packageJson = require ("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
+
+        var actualVersion = process.version;
+        if (semver.neq(expectedVersion, actualVersion)) {
+            fail("Incorrect Node version: expected " + expectedVersion + " but was " +  actualVersion);
+
+        }
+
+
+    });
+
 }())
